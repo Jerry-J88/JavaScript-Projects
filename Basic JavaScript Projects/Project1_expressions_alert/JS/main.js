da4f6b566fda25675a21515b878108e5ca0eb755
Jerry-J88 <jerryjensen88@gmail.com>
2023-03-13 19:02:40 -0600

    Basic JavaScript  Projects

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+var A ="This is a string."; //Assigned a string to the var A.
+
+window.alert(A); //Used window alert to call the var A.
+
+document.write('Hello World!');
+
+document.write(A);
+
+document.write(" I can\'t believe the cool things I\'m learning!"); //String using backslashes to add ' or " to the string.
+
+document.write("\"Those who can, do."  //adding two strings together in one.
++ " Those who can\'t, teach.\"");
+
+var B = " My favorite Ninja Turtle is" + " Michaelangelo." //adding two strings together in one.
+document.write(B);
+
+var Blue = "Leonardo", Red = "Raphael", Purple = "Donatello",  //Addiing multiple varibles and giving them values at the same time.
+    Orange = "Michaelangelo"; 
+document.write(Blue);  //Calls the var Blue which will write Leonardo on the screen.
+
+document.write(4*1); //Expression written out for the program to solve.
